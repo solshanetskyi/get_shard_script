823ff8b4f83419f527c6e9d3a075345a0fe05397
Sergii Olshanetskyi <solshanetskyi@freshbooks.com>
2020-07-10 23:19:08 -0400

    Adding validation.

--- get_shard.py
+++ get_shard.py
@@ -28,9 +28,11 @@ arguments = sys.argv
 
 if not arguments or len(arguments) < 2:
     print("SystemId should be provided.")
-    print("Example: python get_shard.py 4444960")
-    print("Example: python get_shard.py 4444960 staging")
-    print("Example: python get_shard.py 4444960 production")
+    print("Examples:")
+    print("\tpython get_shard.py 4444960")
+    print("\tpython get_shard.py 4444960 staging")
+    print("\tpython get_shard.py 4444960 production")
+    exit(1)
 
 try:
     system_id = int(arguments[1])
@@ -38,16 +40,19 @@ except ValueError:
     print('SystemId is invalid')
     exit(1)
 
+environment = "production"
+
 if len(arguments) > 2:
     environment = arguments[2]
 
 config_path = production_path
 
+if environment not in ["staging", "production"]:
+    print("Environment is invalid. Allowed values: 'staging', 'production'")
+    exit(1)
+
 if environment == "staging":
     config_path = staging_path
-    print('Getting shard name for STAGING...')
-else:
-    print('Getting shard name for PRODUCTION...')
 
 with open(config_path, 'r') as file:
     content = file.read()
@@ -71,7 +76,7 @@ with open(config_path, 'r') as file:
 
     for shard in shards:
         if shard.min_system <= system_id <= shard.max_system:
-            print(shard.name)
+            print(f"System Id: {system_id}, Shard: {shard.name}, Environment: {environment}")
             exit(0)
 
     print('Unable to find the shard')
